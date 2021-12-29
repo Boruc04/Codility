cdfdec523fd1d7c71c379b24a3c357e8bb05bd88
Work <mborucinski@kpmg.pl>
2021-12-29 16:01:23 +0100

    fixing min max problem

--- Lessons/Lesson 4/missing_integer.cs
+++ Lessons/Lesson 4/missing_integer.cs
@@ -8,7 +8,7 @@ using System;
 class Solution {
     public int solution(int[] A) {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
-        var max_value = 1000000; // 1 000 000
+        var max_value = 1000001; // 1 000 000 elements
         var sortedA = new bool[max_value];
         
         for (int i = 0; i<A.Length; i++){
