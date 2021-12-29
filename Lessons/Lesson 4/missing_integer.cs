39e7e40623727300b3d8a5c30dbb324079542b2d
Work <mborucinski@kpmg.pl>
2021-12-29 15:56:09 +0100

    test ok

--- Lessons/Lesson 4/missing_integer.cs
+++ Lessons/Lesson 4/missing_integer.cs
@@ -8,6 +8,19 @@ using System;
 class Solution {
     public int solution(int[] A) {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
+        var max_value = 1000000; // 1 000 000
+        var sortedA = new bool[max_value];
         
+        for (int i = 0; i<A.Length; i++){
+            var value = A[i];
+            if(value <= 0) continue;
+            sortedA[value] = true;     
+        }
+
+        for (int j = 1; j < sortedA.Length; j++){
+            if (sortedA[j] == false) return j;
+        }
+
+        return 1;
     }
 }
