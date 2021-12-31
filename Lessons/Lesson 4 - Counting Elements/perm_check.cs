88fca6c7be3dac6bca06732ef0670afae010b8cf
Work <mborucinski@kpmg.pl>
2021-12-31 09:38:42 +0100

    lesson 4 passing cars

--- Lessons/Lesson 4 - Counting Elements/perm_check.cs
+++ Lessons/Lesson 4 - Counting Elements/perm_check.cs
@@ -9,16 +9,30 @@ class Solution {
     public int solution(int[] A) {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
 
-        var permutation = new bool[A.Length];
-        for (int i = 0; i<A.Length; i++){
-            var value = A[i];
+        var numberOfZeros = 0;
+        var numberOfOnes = 0;
+        var numberOfZerosSmallerThanOnes = 0;
 
-            if(value <= A.Length && permutation[value-1] == false){
-                permutation[value-1] = true;
-            }else{
-                return 0;
+        foreach (var element in A){
+            if(element == 0){
+                numberOfZeros++;
+                numberOfZerosSmallerThanOnes += numberOfOnes;
+            }else if(element == 1){
+                numberOfOnes++;
+            }else {
+                throw new Exception("Invalid number");
             }
         }
-        return 1;
+
+        long numberOfCombinations = Math.Abs((numberOfOnes * numberOfZeros) - numberOfZerosSmallerThanOnes);
+        int result;
+
+        if (numberOfCombinations > 1000000000){
+            result = -1;            
+        }else {
+            result = (int) numberOfCombinations;
+        }
+
+        return result;
     }
 }
